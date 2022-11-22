d776a9f48eade176d5b2a39d2e44aad3bd6dfd6f
JK1320 <kaurjus1@gmail.com>
2022-11-22 23:13:32 +0000

    import Table component & Add filter function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 import './App.css';
+import Table from './Table';
 import {Users} from "./users";
 
 function App() {
   const [query, setQuery] = useState("");
 
- 
+ const search = (data) => {
+return data.filter(item => item.first_name.toLowerCase().includes(query))
+ };
+
+
   return (
     <div className="App">
       <input type="text" placeholder="Search..." className="search" onChange={e => setQuery(e.target.value)} />
-      <ul className="list">
-        {Users.filter(user => user.first_name.toLowerCase().includes(query)).map((user) => (
-          <li key={user.id} className="listItem">{user.first_name}</li>
-        ))}
-      </ul>
+     <Table data={search(Users)} />
     </div>
   );
 }
