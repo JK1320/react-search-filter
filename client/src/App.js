b0b7fe182cf76b4658dbf69f7b190ff5d0b08c9f
JK1320 <kaurjus1@gmail.com>
2022-11-29 00:56:21 +0000

    rearranged code

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import Table from './Table';
-import {Users} from "./users";
+
 
 function App() {
   const [query, setQuery] = useState("");
 
-  const keys = ["first_name", "last_name", "email"]
-
- const search = (data) => {
-return data.filter((item) =>
-    keys.some((key) => item[key].toLowerCase().includes(query))
-);
- };
-
-
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." className="search" onChange={e => setQuery(e.target.value)} />
-     <Table data={search(Users)} />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={[]} />
     </div>
   );
 }
