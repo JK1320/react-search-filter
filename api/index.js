e48585520e6af376a898cbaf44d192353f92d7b8
JK1320 <kaurjus1@gmail.com>
2022-12-02 00:25:05 +0000

    tested; return first 10 items

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const Users = require("./users");
 app.use(cors());
 
 app.get("/", (req, res) => {
-res.json(Users);
+res.json(Users.splice(0, 10));
 });
 
 app.listen(5001, () => console.log("API IS WORKING"));
