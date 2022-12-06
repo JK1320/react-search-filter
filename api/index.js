0486b6a41ef1e0162beabab9077fe028e0057892
JK1320 <kaurjus1@gmail.com>
2022-12-06 12:11:19 +0000

    add filter function to filter data

--- api/index.js
+++ api/index.js
@@ -6,7 +6,16 @@ const Users = require("./users");
 app.use(cors());
 
 app.get("/", (req, res) => {
-res.json(Users.splice(0, 10));
+    const {q} = req.query;
+    
+const keys = ["first_name", "last_name", "email"];
+
+const search = (data) => {
+  return data.filter((item) => 
+  keys.some((key) => item[key].toLowerCase().includes(q)));
+};
+
+res.json(search(Users).splice(0, 10));
 });
 
 app.listen(5001, () => console.log("API IS WORKING"));
